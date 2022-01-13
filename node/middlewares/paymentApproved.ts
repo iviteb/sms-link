7c684138c0c7d3c571eeba1216cc4666c9ce9689
Marian Ene <marian.ene@customsoft.ro>
2022-01-13 10:50:46 +0200

    added Marketplace domain filters

--- node/middlewares/paymentApproved.ts
+++ node/middlewares/paymentApproved.ts
@@ -13,40 +13,42 @@ export async function paymentApproved(
     clients: { oms, sms },
   } = ctx
 
-  const settings = await getSettings(ctx)
+  if (ctx.body.domain === 'Marketplace') {
+    const settings = await getSettings(ctx)
 
-  if (
-    settings.allowPaymentAuthorizedSMS &&
-    settings.onlinePaymentAuthorizedText &&
-    isMessageValid(settings.onlinePaymentAuthorizedText)
-  ) {
-    const order = await oms.order(ctx.body.orderId)
+    if (
+      settings.allowPaymentAuthorizedSMS &&
+      settings.onlinePaymentAuthorizedText &&
+      isMessageValid(settings.onlinePaymentAuthorizedText)
+    ) {
+      const order = await oms.order(ctx.body.orderId)
 
-    if (order != null) {
-      const message = formatMessage(settings.onlinePaymentAuthorizedText, order)
+      if (order != null) {
+        const message = formatMessage(settings.onlinePaymentAuthorizedText, order)
 
-      // Send message to user
-      await sms.sendJsonMessage({
-        connection_id: settings.connectionID,
-        password: settings.password,
-        to: order.clientProfileData.phone,
-        message,
-        test: settings.enableSandbox ? 1 : 0,
-      })
-    } else {
-      logEvent('Critical', 'Error', 'get-order-info', {
+        // Send message to user
+        await sms.sendJsonMessage({
+          connection_id: settings.connectionID,
+          password: settings.password,
+          to: order.clientProfileData.phone,
+          message,
+          test: settings.enableSandbox ? 1 : 0,
+        })
+      } else {
+        logEvent('Critical', 'Error', 'get-order-info', {
+          account: ctx.vtex.account,
+          message: `Invalid Order data received for orderId=${ctx.body.orderId} !`,
+          sourceType: 'LOG',
+        })
+      }
+    } else if (settings.allowPaymentAuthorizedSMS) {
+      logEvent('Critical', 'Error', 'get-settings', {
         account: ctx.vtex.account,
-        message: `Invalid Order data received for orderId=${ctx.body.orderId} !`,
+        message: 'Empty <message> or invalid character detected!',
         sourceType: 'LOG',
       })
     }
-  } else if (settings.allowPaymentAuthorizedSMS) {
-    logEvent('Critical', 'Error', 'get-settings', {
-      account: ctx.vtex.account,
-      message: 'Empty <message> or invalid character detected!',
-      sourceType: 'LOG',
-    })
   }
-
+  
   await next()
 }
--- node/middlewares/paymentDenied.ts
+++ node/middlewares/paymentDenied.ts
@@ -13,40 +13,42 @@ export async function paymentDenied(
     clients: { oms, sms },
   } = ctx
 
-  const settings = await getSettings(ctx)
+  if (ctx.body.domain === 'Marketplace') {
+    const settings = await getSettings(ctx)
 
-  if (
-    settings.allowPaymentErrorSMS &&
-    settings.onlinePaymentErrorText &&
-    isMessageValid(settings.onlinePaymentErrorText)
-  ) {
-    const order = await oms.order(ctx.body.orderId)
+    if (
+      settings.allowPaymentErrorSMS &&
+      settings.onlinePaymentErrorText &&
+      isMessageValid(settings.onlinePaymentErrorText)
+    ) {
+      const order = await oms.order(ctx.body.orderId)
 
-    if (order != null) {
-      const message = formatMessage(settings.onlinePaymentErrorText, order)
+      if (order != null) {
+        const message = formatMessage(settings.onlinePaymentErrorText, order)
 
-      // Send message to user
-      await sms.sendJsonMessage({
-        connection_id: settings.connectionID,
-        password: settings.password,
-        to: order.clientProfileData.phone,
-        message,
-        test: settings.enableSandbox ? 1 : 0,
-      })
-    } else {
-      logEvent('Critical', 'Error', 'get-order-info', {
+        // Send message to user
+        await sms.sendJsonMessage({
+          connection_id: settings.connectionID,
+          password: settings.password,
+          to: order.clientProfileData.phone,
+          message,
+          test: settings.enableSandbox ? 1 : 0,
+        })
+      } else {
+        logEvent('Critical', 'Error', 'get-order-info', {
+          account: ctx.vtex.account,
+          message: `Invalid Order data received for orderId=${ctx.body.orderId} !`,
+          sourceType: 'LOG',
+        })
+      }
+    } else if (settings.allowPaymentErrorSMS) {
+      logEvent('Critical', 'Error', 'get-settings', {
         account: ctx.vtex.account,
-        message: `Invalid Order data received for orderId=${ctx.body.orderId} !`,
+        message: 'Empty <message> or invalid character detected!',
         sourceType: 'LOG',
       })
     }
-  } else if (settings.allowPaymentErrorSMS) {
-    logEvent('Critical', 'Error', 'get-settings', {
-      account: ctx.vtex.account,
-      message: 'Empty <message> or invalid character detected!',
-      sourceType: 'LOG',
-    })
   }
-
+  
   await next()
 }
